51e1e9ae26874ff71925d6a61f08766f3bd9e751
kaaaata <catherinehan714@gmail.com>
2018-03-28 21:09:34 -0700

    Clean code

--- src/Box.js
+++ src/Box.js
@@ -18,11 +18,7 @@ export default connect(mapStateToProps, mapDispatchToProps)(class Box extends Co
     const y = e.nativeEvent.offsetY - 8;
     // the box is 158px by 158px, the extra 8px is to align the happiness/productivity dart with the cursor pointer tip.
     // it is > 0 instead of >= 0 to avoid some weird glitch with double clicking the dart causing x=0, y=0
-    if (x > 0 && y > 0 && x <= 150 && y <= 150) {
-      console.log('x = ', x);
-      console.log('y = ', y);
-      updateActivity({ happiness: y, productivity: x })
-    };
+    if (x > 0 && y > 0 && x <= 150 && y <= 150) updateActivity({ happiness: y, productivity: x });
   }
 
   render() {
